10ec7572711a6b296dbbd9cb5ea3e6fb249908ff
Man836 <128714178+Man836@users.noreply.github.com>
2025-08-14 09:13:21 +0000

    commit config done

---
+++ 01_basics/converstionOperation.js
@@ -0,0 +1,34 @@
+// let score = null // op: 0
+// let score = undefined // op: NaN
+// let score = "33ab" // op: Nan 
+// let score = true // op: 1
+// let score = "manisha" // op: NaN
+// let score = 33 
+
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// -------------------------------------------------------
+// let isLoggedIn = 1
+// let isLoggedIn = "" //op: false after coverstion
+let isLoggedIn = "manisha" //op: true
+let booleanisLoggedIn = Boolean(isLoggedIn)
+
+
+console.log(typeof isLoggedIn); // op: number
+console.log(isLoggedIn); // op: 1
+
+console.log(typeof booleanisLoggedIn); // boolean
+console.log(booleanisLoggedIn); // op: true
+
+
+
+
+
+
+
+
